2fc0d3d054e801a8f6aecc70e917131c372895fa
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-20 23:47:44 +0200

    improve tests

--- test2.js
+++ test2.js
@@ -1,9 +1,10 @@
 'use strict';
 var test = require('ava');
 var processExists = require('process-exists');
+var fkill = require('fkill');
 
 test('persistent option', function (t) {
-	t.plan(4);
+	t.plan(5);
 
 	processExists('noop-process-1', function (err, exists) {
 		t.assert(!err, err);
@@ -13,5 +14,9 @@ test('persistent option', function (t) {
 	processExists('noop-process-2', function (err, exists) {
 		t.assert(!err, err);
 		t.assert(exists);
+
+		fkill('noop-process-2', function (err) {
+			t.assert(!err, err);
+		});
 	});
 });
